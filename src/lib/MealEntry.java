3a003e30a36df433efecba6cf36586aa0abfff38
Nick Adams <150203946+creatoradams@users.noreply.github.com>
2025-04-30 01:51:36 -0500

    Update MealEntry.java

--- src/lib/MealEntry.java
+++ src/lib/MealEntry.java
@@ -4,8 +4,9 @@ package lib;
  * MealEntry represents a single meal's nutritional data
  * including calories and macronutrients, along with the date and name.
  */
-public class MealEntry {
-    public final String date;        // Date the meal was logged (yyyy-MM-dd)
+public class MealEntry
+{
+    public final String date;        // Date the meal was logged (MM-dd-yyyy)
     public final String mealName;    // Name of the meal
     public final int calories;       // Total calories in the meal
     public final int protein;        // Protein in grams
@@ -15,7 +16,8 @@ public class MealEntry {
     /**
      * Constructs a new MealEntry with all required nutritional details.
      */
-    public MealEntry(String date, String mealName, int calories, int protein, int carbs, int fats) {
+    public MealEntry(String date, String mealName, int calories, int protein, int carbs, int fats)
+    {
         this.date = date;
         this.mealName = mealName;
         this.calories = calories;
@@ -28,7 +30,8 @@ public class MealEntry {
      * Converts the meal entry into a String array for table display.
      * Order matches table columns: Date, Meal Name, Calories, Protein, Carbs, Fats.
      */
-    public String[] toTableRow() {
+    public String[] toTableRow()
+    {
         return new String[]{
                 date, mealName,
                 String.valueOf(calories),
